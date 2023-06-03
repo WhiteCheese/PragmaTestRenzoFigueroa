480540557a6d84dfc10651169729c05f5016d2e5
Renzo Figueroa <renzofigueroa1999@gmail.com>
2023-06-03 14:22:29 -0500

    Initial Commit

---
+++ Pragma-Test/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Pragma-Test
+//
+//  Created by Renzo Rodrigo Figueroa Fernández on 3/06/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
